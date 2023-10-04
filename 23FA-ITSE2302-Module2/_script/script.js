47dbf49f08f519b2201c67e673d4e6b85f7ebbe0
Stacey Nash <stacey77nash@gmail.com>
2023-10-04 02:41:20 -0500

    Uploaded folder for Module 2
    Removed all practice files and notes from Module 2 folder. 
    Updated html file with new script, and commented out Module 1 script. 
    Added new stringsOperatorsLoops.js file.

---
+++ 23FA-ITSE2302-Module2/_script/script.js
@@ -0,0 +1,29 @@
+/* NAME: Stacey Nash
+   COURSE: ITSE-2302 - Intermediate Web Programming
+   DATE: 21 September 2023
+   DESCRIPTION: Module 1 project using basic Javascript and uploading to GitHub. */
+
+
+   // Assign the given values to the created variables.
+   var firstName = "Stacey";
+   var lastName = "Nash";
+   var catsAreCool = "I love that cats always land on their feet."; 
+   var theTruth = true;
+   var num_10 = 10;
+   var num_15 = 15;
+   var num_25 = 25;
+
+   // Print using concatenation to displ fname and lname with a space
+    console.log(firstName + " " + lastName);
+
+    // Print out length of sentence
+    console.log("The length of the sentence is: " + catsAreCool.length + " characters long.");
+
+    // Prints out a sentence using concatenation
+    console.log("The sum of " + num_10 + " and " + num_15 + " is equal to " + num_25 + ".");
+
+    // Prints a sentence using 'typeOf' attribute 
+    console.log("The variable theTruth is storing the value " + theTruth + ". The data type of theTruth is " + typeof theTruth + ".");
+
+
+
